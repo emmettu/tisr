c930eeea4b9d531596f9152a8250a26e496c4328
Emmett Underhill <emmettu@gmail.com>
2015-10-15 22:43:05 -0400

    Make script more readable

--- othello.py
+++ othello.py
@@ -1,11 +1,5 @@
 import sys
-f = open(sys.argv[1], 'r')
-c = f.read()
-n = ""
-for i in c:
-    if i.isalpha():
-        n += str(ord(i))
-s = str(int(str((int(n) + 1) * 2004582342310635843922)[-22:]) + 2907326189180680668794)
+s = str(int(str((int(''.join([str(ord(i)) for i in open(sys.argv[1], 'r').read() if i.isalpha()])) + 1) * 2004582342310635843922)[-22:]) + 2907326189180680668794)
 i = 0
 while i < 22:
     sys.stdout.write(chr(int(s[i:i+2])))
